97ed8c864158dc81546d9ce7cedb721950e95f92
Daniar <tbylat@gmail.com>
2022-10-10 03:12:20 +0500

    homework

---
+++ Program task 13.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая выводит третью цифру СЛЕВА заданного числа или сообщает, что третьей цифры нет.
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+//  if (num >= 100 && num < 1000 )
+//  {
+//     int num1 = num % 100;
+//     int num2 = num1 % 10;
+    
+//     Console.WriteLine(num2);
+//  }
+//  else if (num < 100)
+//  {
+//     Console.WriteLine("Третьей цифры нет");
+//  } 
+//  else if (num >=1000 && num < 10000)
+//  {
+//     int num3 = num % 1000;
+//     int num4 = num3 % 100;
+//     int num5 = num4 / 10;
+//     Console.WriteLine(num5);
+//  }
+//   else if (num >=10000 && num < 100000)
+//  {
+//     int num6 = num % 10000;
+//     int num7 = num6 % 1000;
+//     int num8 = num7 / 100;
+//     Console.WriteLine(num8);
+//  }
+//   else if (num >=100000 && num < 1000000)
+//  {
+//     int num9 = num % 100000;
+//     int num10 = num9 / 1000;
+//     int num11 = num10 % 10;
+//     Console.WriteLine(num11);
+//  }
+ 
+ 
---
+++ Program task 15.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// Console.WriteLine("Введите число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num <= 7 && num > 0)
+// {
+//     if (num==1)
+//     { 
+//         Console.WriteLine("Сегодня у нас Понедельник");
+//     }
+//      else if (num==2)
+//     { 
+//         Console.WriteLine("Сегодня у нас Вторник");
+//     }
+//      else if (num==3)
+//     { 
+//         Console.WriteLine("Сегодня у нас Среда");
+//     }
+//      else if (num==4)
+//     { 
+//         Console.WriteLine("Сегодня у нас Четверг");
+//     }
+//      else if (num==5)
+//     { 
+//         Console.WriteLine("Сегодня у нас Пятница");
+//     }
+//      else if (num==6)
+//     { 
+//         Console.WriteLine("Сегодня у нас Суббота, а значит выходной!");
+//     }
+//      else if (num==7)
+//     { 
+//         Console.WriteLine("Сегодня у нас Воскресенье, а значит выходной!");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Такого дня недели нет");
+// }
+
+
